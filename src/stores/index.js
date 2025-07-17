cac851b1751fee67d54a012046389f09c28b141b
asilbek_tursun <148640148+AsilbekTursunov@users.noreply.github.com>
2025-07-17 11:14:17 +0500

    delete registration

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,34 +2,34 @@ import { createLogger, createStore } from 'vuex'
 import { decodeJWT } from '@/utils/decodeJWT'
 const debug = import.meta.env.VITE_APP_API_URL !== 'production'
 // main page which always to be active 
-const home = { 
+const home = {
   index: '0',
   path: '/',
   title: '메인',
   label: '나의 현황',
-  icon: 'home_8', 
+  icon: 'home_8',
 }
 
 const plugins = createLogger({
   collapsed: true,
   logActions: true, // Log Actions
   logMutations: true, // Log mutations
-  logger: console, 
+  logger: console,
 })
 
 
 const store = createStore({
   strict: debug,
   plugins: debug ? [plugins] : [],
-  state: { 
-    token: JSON.parse(localStorage.getItem('user_token')) || null,
+  state: {
+    token: JSON.parse(localStorage.getItem('user_token')) || 'user',
     isCollapsed: true,
     tabs: JSON.parse(localStorage.getItem('tabs')) || [home],
     activeTab: JSON.parse(localStorage.getItem('activeTab')) || home
   },
   getters: {
     getAuthInfo(state) {
-      if(!state.token) return null
+      if (!state.token) return null
 
       const getUserInfo = decodeJWT(state.token)
 
@@ -50,12 +50,12 @@ const store = createStore({
     setCollapseSidebar(state, collapse) {
       state.isCollapsed = collapse
     },
-    setTabs(state, tab) {  
-      const existTab = state.tabs.filter(nowTab => nowTab.label == tab.label)  
-      if (existTab.length == 0 && tab.path != '/') { 
+    setTabs(state, tab) {
+      const existTab = state.tabs.filter(nowTab => nowTab.label == tab.label)
+      if (existTab.length == 0 && tab.path != '/') {
         state.tabs.push(tab)
         localStorage.setItem('tabs', JSON.stringify(state.tabs))
-      }  
+      }
     },
     removeTab(state, tab) {
       state.tabs = state.tabs.filter(nowTab => nowTab.label != tab.label)
@@ -69,14 +69,14 @@ const store = createStore({
       localStorage.setItem('user_token', JSON.stringify(token))
       state.token = token
     },
-    setClearTabs(state) { 
+    setClearTabs(state) {
       state.tabs = [home]
       localStorage.removeItem('tabs')
     },
     logOut(state) {
-      state.token = null 
+      state.token = null
       state.tabs = [home]
-      state.activeTab = home 
+      state.activeTab = home
       localStorage.clear()
     }
   },
